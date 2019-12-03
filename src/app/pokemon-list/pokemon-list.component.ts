6e1d3e58096e76464cfb5597755232191d03cd50
jimdeopante <54925378+jimdeopante@users.noreply.github.com>
2019-12-03 23:00:30 +0800

    "committed"

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -8,8 +8,18 @@ import { Pokemons } from '../pokemons';
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
+
 export class PokemonListComponent implements OnInit {
 pokemons;
+next;
+
+i = 0;
+imgsrc = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+extsn = ".png"
+
+//ctr = this.i++;
+
+
 
   constructor(private nameservice: NameService) { }
 
@@ -18,8 +28,18 @@ pokemons;
     .subscribe((data: Pokemons[]) => {
       // console.log('Data', data);
       this.pokemons = data.results;
-      // console.log(this.pokemons);
+      this.next = data.next.results;
+      console.log(this.next);
+      // (this.ctr);
     });
   }
+  
+  // nextBtn() {
+  //   this.nameservice.getPokemon()
+  //   .subscribe((data: Pokemons[]) => {
+  //     this.next = data.next;
+  //     console.log(this.next);
+  //   });
+  // }
 
 }
